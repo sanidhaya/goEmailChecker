dab4179941bad64c8ffc8ef04dce20657716a4e3
Sanidhaya <Sanidhaya08sharma11@gmail.com>
2024-02-26 18:49:22 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sanidhaya/goEmailChecker
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func CheckDomain(domain string) {
+	var hasMx, hasSPF, hasDMARC bool
+	var spfRecord, dmarcRecord string
+
+	mxRecords, err := net.LookupMX(domain)
+
+	if err != nil {
+		log.Printf("Error %v\n,", err)
+	}
+	if len(mxRecords) > 0 {
+		hasMx = true
+	}
+
+	txtRecords, err := net.LookupTXT(domain)
+	if err != nil {
+		log.Printf("Error %v\n", err)
+	}
+	for _, record := range txtRecords {
+		if strings.HasPrefix(record, "v=spf1") {
+			hasSPF = true
+			spfRecord = record
+			break
+		}
+	}
+
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		log.Printf("Error %v\n", err)
+	}
+	for _, record := range dmarcRecords {
+		if strings.HasPrefix(record, "v=DMARC1") {
+			hasDMARC = true
+			dmarcRecord = record
+			break
+		}
+	}
+
+	fmt.Printf("%v, %v , %v , %v , %v , %v ", domain, hasMx, hasSPF, spfRecord, hasDMARC, dmarcRecord)
+
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("domain, hasMX, hasSPF, spfRecord, has DMARC,dmarcRecord \n")
+
+	for scanner.Scan() {
+		CheckDomain(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error could not read from input : %v\n", err)
+	}
+}
